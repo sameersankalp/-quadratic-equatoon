788d3f530d93522fddeab5727e299a1cac2b206a
sameersankalp <47040878+sameersankalp@users.noreply.github.com>
2019-12-19 00:54:15 +0700

    Add files via upload

---
+++ quadratic eqaution.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(int argc, char const *argv[])
+{
+    int a,b,c;
+    float r1,r2,d;
+    printf("entrevalue of a");
+    scanf("%d",&a);
+    printf("antre b");
+    scanf("%d",&b);
+    printf("enrre c ");
+    scanf("%d",&c);
+    d=(b*b)-(4*a*c);
+    if (d>0)
+    {
+       printf(" real roots");
+
+       r1=((-b)+sqrt(d))/(2*a);
+       r2=((-b)-sqrt(d))/(2*a);
+       printf("\nroots:%f,%f\n",r1,r2);
+
+
+
+    }
+
+else if (d==0)
+{
+   printf("\nroots are equal\n");
+
+
+}
+
+
+else if(d<0)
+
+{
+
+    printf("bhag yahan se");
+
+
+
+}
+    return 0;
+}
